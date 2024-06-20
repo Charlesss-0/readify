4fb21a66e7a804a462b1e44c43a56d21f7439ccb
Charlesss-0 <aragonaugusto588@gmail.com>
2024-06-20 04:25:03 +0000

    add(dropdownContent): renders a list of items
    - component to render the list of items when the dropdown is open

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import DropdownContent from './dropdownContent'
 import Header from './header'
 import Sidebar from './sidebar'
 import UploadBtn from './uploadBtn'
+import DropdownContent from './dropdownContent'
 
 export { BookCollection, UploadBtn, Sidebar, Header, DropdownContent }
