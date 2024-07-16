61fde46c22e350dd8eaee55206360985cea013c7
Charlesss-0 <aragonaugusto588@gmail.com>
2024-07-15 22:44:53 -0600

    refactor(alerts): improve animation effect

--- src/components/ui/alerts.tsx
+++ src/components/ui/alerts.tsx
@@ -1,6 +1,28 @@
 import { AppDispatch, RootState, appSlice } from '@/src/lib'
+import styled, { keyframes } from 'styled-components'
 import { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
+const show = keyframes`
+    0% {
+        transform: translateX(-50%) translateY(200%);
+		opacity: 0;
+    }
+    100% {
+        transform: translateX(-50%) translateY(0%);
+		opacity: 1;
+    }
+`
+
+const Alert = styled.div`
+	position: absolute;
+	bottom: 0.5rem;
+	left: 50%;
+	transform: translateX(-50%);
+	color: #ffffff;
+	width: max-content;
+	animation: ${show} 500ms forwards;
+`
 
 export default function Alerts() {
 	const dispatch = useDispatch<AppDispatch>()
@@ -27,13 +49,8 @@ export default function Alerts() {
 
 	return (
 		<>
-			{showAlert && fileState === 'uploaded' && (
-				<div
-					role="alert"
-					className={`alert alert-success text-white w-max absolute bottom-2 left-[50%] translate-x-[-50%] transition-all duration-200 ${
-						showAlert ? 'opacity-100' : 'opacity-0'
-					}`}
-				>
+			{showAlert && (
+				<Alert role="alert" className="alert alert-success">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						className="h-6 w-6 shrink-0 stroke-current"
@@ -47,32 +64,8 @@ export default function Alerts() {
 							d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
 						/>
 					</svg>
-					<span>File uploaded successfully!</span>
-				</div>
-			)}
-
-			{showAlert && fileState === 'deleted' && (
-				<div
-					role="alert"
-					className={`alert alert-success text-white w-max absolute bottom-2 left-[50%] translate-x-[-50%] transition-all duration-200 ${
-						showAlert ? 'opacity-100' : 'opacity-0'
-					}`}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6 shrink-0 stroke-current"
-						fill="none"
-						viewBox="0 0 24 24"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-						/>
-					</svg>
-					<span>File deleted successfully!</span>
-				</div>
+					<span>File {fileState} successfully!</span>
+				</Alert>
 			)}
 		</>
 	)
