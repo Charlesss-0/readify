cb2ccb86e8f817901646c4c17c48e2ff9235280b
Charlesss-0 <aragonaugusto588@gmail.com>
2024-07-23 00:19:34 -0600

    refactor(header): update search icon color and size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { IoIosSearch } from 'react-icons/io'
+import { theme } from '../constants'
 
 export default function Header() {
 	return (
 		<header className="flex bg-base-100 fixed top-0 left-0 w-full z-10 shadow-md">
-			<div className="flex justify-end w-full p-2 px-[1rem]">
+			<div className="flex justify-end w-full p-[0.65rem] px-[1rem]">
 				<button>
-					<IoIosSearch className="w-10 h-10" />
+					<IoIosSearch className="w-10 h-10" fill={`${theme.neutral}`} />
 				</button>
 			</div>
 		</header>
