bdff3f74dce74902474d4119354e5b35539497da
Charlesss-0 <aragonaugusto588@gmail.com>
2024-07-23 00:01:18 -0600

    refactor(header): add shadow effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { IoIosSearch } from 'react-icons/io'
 
 export default function Header() {
 	return (
-		<header className="flex bg-base-100 fixed top-0 left-0 w-full z-10">
+		<header className="flex bg-base-100 fixed top-0 left-0 w-full z-10 shadow-md">
 			<div className="flex justify-end w-full p-2 px-[1rem]">
 				<button>
 					<IoIosSearch className="w-10 h-10" />
